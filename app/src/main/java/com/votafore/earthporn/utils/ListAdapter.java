7fd6533fb9d40e1653bf4735b8622f8cb49e4c5b
votafore <a.zakharov.druzh@gmail.com>
2018-02-21 10:46:55 +0200

    adapter v 1.0

---
+++ app/src/main/java/com/votafore/earthporn/utils/ListAdapter.java
@@ -0,0 +1,33 @@
+package com.votafore.earthporn.utils;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>{
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
